ed4f527a51d40aa8639c5d28da97268d12c003cf
gtrb <guillaume.trb@hotmail.com>
2019-05-06 13:56:34 +0200

    fixed clickable desktop project menu

--- src/JS/main.js
+++ src/JS/main.js
@@ -1,5 +1,6 @@
 const slider = document.querySelector('.slider__container');
 const slides =  document.querySelectorAll('.slide');
+const buttons = document.querySelectorAll('.button');
 const dots = document.querySelectorAll('.projects__dot');
 const allNode = document.querySelector('.projects__menu');
 const menuScreen = document.querySelector('.menuScreen');
@@ -7,73 +8,18 @@ const menuCross = document.querySelector('.exit');
 const body = document.querySelector('body');
 const html = document.querySelector('html');
 
-/* const render = num => {
-  body.classList.remove('menu--open');
-  html.classList.remove('menu--open');
+//desktop menu images
+const images = document.querySelectorAll('.slide .slide__image');
+console.log(images);
 
-  for (let slide of slides){
-    slide.classList.remove('visible');
-  }
-  for (let dot of dots){
-    dot.classList.remove('active');
-  }
-  slides[num].classList.add('visible');
-  dots[num].classList.add('active');
+for (let button of buttons){
+  button.addEventListener('click', e => {
+    let visibleEl = document.querySelector('.visible');
+    visibleEl.classList.remove('visible');
+    let image = images[e.currentTarget.id];
+    image.classList.add('visible');
+  })
 }
-
-const change = () => {
-  let savedState = localStorage.getItem('savedState');
-  switch (window.location.hash){
-    case '#projects':
-      if (!savedState){
-        render(0);
-        break;
-      }
-      else {
-        render(savedState);
-        break;
-      }
-    case '#alfred-slide':
-    render(0);
-    localStorage.setItem('savedState', 0);
-    break;
-    case '#umr-slide':
-      render(1);
-      localStorage.setItem('savedState', 1);
-      break;
-    case '#pacman-slide':
-      render(2);
-      localStorage.setItem('savedState', 2);
-      break;
-    case '#morty-slide':
-      render(3);
-      localStorage.setItem('savedState', 3);
-      break;
-    case '#nissin-slide':
-      render(4);
-      localStorage.setItem('savedState', 4);
-      break;
-    case '#socomptoir-slide':
-      render(5);
-      localStorage.setItem('savedState', 5);
-      break;
-    case '#about':
-      if (!savedState){
-        render(0);
-        break;
-      }
-      else {
-        render(savedState);
-        break;
-      }
-    default:
-        window.location.hash = '#alfred-slide';
-      break;
-  }
-}
-
-window.addEventListener('load', change);
-window.addEventListener('hashchange', change); */
 
 //Menu and close menu
 allNode.addEventListener('click', () => {
@@ -100,45 +46,29 @@ function closeMenu () {
 
 //Slider manager
 
-/* const sliderManager = new Hammer.Manager(slider);
-sliderManager.add(new Hammer.Swipe({ threshold: 10, direction: 'DIRECTION_HORIZONTAL', velocity: 0.3}));
-sliderManager.on('swipe', function(e) {
-  console.log(e);
-}); */
 let canMove = true;
 let position = 0;
 slider.appendChild(slides[0].cloneNode(true));
+const secondAlf = document.querySelectorAll('.slider__alfred .slide__image')[1];
+secondAlf.classList.remove('visible');
 const slideCount = slides.length+1;
 dots[position].classList.add('active');
-/* let percentage = 0; */
 
 for (let i=0 ; i<slides.length ; i++){
   const sliderHammer = new Hammer(slides[i]);
   sliderHammer.on('swiperight swipeleft tap', e => {
-    if (e.type != 'tap'){
-    /*   let idPrev = '';
-      let idNext = ''; */
-      
+    if (e.type != 'tap'){      
       switch (e.direction){
         case 2 : 
           console.log('left');
           next();
-         /*  percentage-=100; */
-          /* idNext = slides[i+1] ? slides[i+1].id : slides[0];
-          console.log(idNext);
-          window.location.hash = `#${idNext}`; */
           break;
         case 4 :
           console.log('right');
           previous();
-         /*  percentage+=100; */
-         /*  idPrev = slides[i-1] ? slides[i-1].id : slides[slides.length-1];
-          console.log(idPrev);
-          window.location.hash = `#${idPrev}`; */
           break;
       }
     }
-    /*   slider.style.transform = `translateX(${percentage}vw)`; */
     else{
       let name = slides[i].id.split('-')[0];
       window.location.pathname = `projects/${name}.html`;
